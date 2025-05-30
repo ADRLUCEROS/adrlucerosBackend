f2a733be83fa727a62c35a5773378b7ee90e6fa1
frankly221 <alexis.delcastillo@upeu.edu.pe>
2025-05-29 22:58:40 -0500

    finish para Tienda

--- src/main/java/com/adrLuceros/transporte/Tienda/infraestructura/input/Controllers/TiendaControllers.java
+++ src/main/java/com/adrLuceros/transporte/Tienda/infraestructura/input/Controllers/TiendaControllers.java
@@ -3,12 +3,16 @@ package com.adrLuceros.transporte.Tienda.infraestructura.input.Controllers;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import com.adrLuceros.transporte.Tienda.application.DTO.TiendaDTO;
 import com.adrLuceros.transporte.Tienda.application.DTO.TiendaEmpresaDTO;
 import com.adrLuceros.transporte.Tienda.application.service.TiendaService;
 
@@ -33,5 +37,23 @@ public class TiendaControllers {
         TiendaEmpresaDTO tienda = tiendaService.findTiendaById(idTienda);
         return ResponseEntity.ok(tienda);
     }
+
+    @PostMapping
+    public ResponseEntity<Void> saveTienda(@RequestBody TiendaDTO tiendaDTO) {
+        tiendaService.saveTienda(tiendaDTO);
+        return ResponseEntity.ok().build();
+    }
+    
+    @PutMapping
+    public ResponseEntity<Void> updateTienda(@RequestBody TiendaDTO tiendaDTO) {
+        tiendaService.updateTienda(tiendaDTO);
+        return ResponseEntity.ok().build();
+    }  
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteTienda(@RequestBody TiendaDTO tiendaDTO) {
+        tiendaService.deleteTienda(tiendaDTO);
+        return ResponseEntity.ok().build();
+    }  
     
 }
