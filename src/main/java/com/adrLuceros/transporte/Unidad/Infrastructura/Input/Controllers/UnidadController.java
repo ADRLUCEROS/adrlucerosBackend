34ff2af0a4732271d0eb28ec8f4badb44dcc3869
frankly221 <alexis.delcastillo@upeu.edu.pe>
2025-05-29 22:50:45 -0500

    acabado para unidad

--- src/main/java/com/adrLuceros/transporte/Unidad/Infrastructura/Input/Controllers/UnidadController.java
+++ src/main/java/com/adrLuceros/transporte/Unidad/Infrastructura/Input/Controllers/UnidadController.java
@@ -3,8 +3,12 @@ package com.adrLuceros.transporte.Unidad.Infrastructura.Input.Controllers;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,6 +40,24 @@ public class UnidadController {
         List<UnidadDTO> unidades = unidadService.findAll();
         return ResponseEntity.ok(unidades);
     } 
+
+    @PostMapping
+    public ResponseEntity<Void> save(@RequestBody UnidadDTO unidadDTO) {
+        unidadService.save(unidadDTO);
+        return ResponseEntity.ok().build();
+    }   
+
+        @DeleteMapping
+    public ResponseEntity<Void> delete(@RequestBody UnidadDTO unidadDTO) {
+        unidadService.delete(unidadDTO);
+        return ResponseEntity.ok().build();
+    }
+
+        @PutMapping
+    public ResponseEntity<Void> update(@RequestBody UnidadDTO unidadDTO) {
+        unidadService.update(unidadDTO);
+        return ResponseEntity.ok().build();
+    }
 
 
     
