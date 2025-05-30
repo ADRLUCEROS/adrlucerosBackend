d03513b601ea212ca709d7707f40a8370be329ae
frankly221 <alexis.delcastillo@upeu.edu.pe>
2025-05-29 20:45:04 -0500

    endpoints terminados

--- src/main/java/com/adrLuceros/transporte/Empresa/Infrastructura/Input/Controllers/EmpresaControllers.java
+++ src/main/java/com/adrLuceros/transporte/Empresa/Infrastructura/Input/Controllers/EmpresaControllers.java
@@ -3,8 +3,12 @@ package com.adrLuceros.transporte.Empresa.Infrastructura.Input.Controllers;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +35,23 @@ public class EmpresaControllers {
         List<EmpresaDTO> empresas = empresaService.findAll();
         return ResponseEntity.ok(empresas);
     }
+
+    @PostMapping
+    public ResponseEntity<Void> saveEmpresa(@RequestBody EmpresaDTO empresaDTO) {
+        empresaService.save(empresaDTO);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> updateEmpresa(@RequestBody EmpresaDTO empresaDTO) {
+        empresaService.update(empresaDTO);
+        return ResponseEntity.ok().build();
+    } 
+    
+    @DeleteMapping
+    public ResponseEntity<Void> deleteEmpresa(@RequestBody EmpresaDTO empresaDTO) {
+        empresaService.delete(empresaDTO);
+        return ResponseEntity.ok().build();
+    }   
     
 }
