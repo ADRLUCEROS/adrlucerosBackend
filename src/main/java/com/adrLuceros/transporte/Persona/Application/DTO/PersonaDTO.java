48262fec2990b614dc58e7c2e91bbdfb3c16464d
frankly221 <alexis.delcastillo@upeu.edu.pe>
2025-06-01 17:28:47 -0500

    avance para persona

--- src/main/java/com/adrLuceros/transporte/Persona/Application/DTO/PersonaDTO.java
+++ src/main/java/com/adrLuceros/transporte/Persona/Application/DTO/PersonaDTO.java
@@ -1,5 +1,28 @@
 package com.adrLuceros.transporte.Persona.Application.DTO;
 
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class PersonaDTO {
+    private int idPersona;
+    private String nombres;
+    private String apellidos;
+    private int numDoc;
+    private int numContacto;
+    private int numWpp;
+    private LocalDate fechaNac; 
+    private String correo;
+    private String direccion;
+    private String sexo;
+    private int cargoIdCargo;
     
 }
---
+++ src/main/java/com/adrLuceros/transporte/Persona/Application/Mapper/PersonMapperModelAndDTO.java
@@ -0,0 +1,35 @@
+package com.adrLuceros.transporte.Persona.Application.Mapper;
+
+import org.modelmapper.ModelMapper;
+import com.adrLuceros.transporte.Persona.Domain.Modal.Persona;
+import com.adrLuceros.transporte.Persona.Application.DTO.PersonaDTO;
+
+public class PersonMapperModelAndDTO {
+
+    
+    private final ModelMapper modelMapper;
+
+
+        public PersonMapperModelAndDTO(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+        // Configurar para ignorar campos null al mapear
+        this.modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setSkipNullEnabled(true);
+    }
+    public Persona DTOToModel(PersonaDTO personaDTO) {
+        return modelMapper.map(personaDTO, Persona.class);
+    }   
+
+    public PersonaDTO ModelToDTO(Persona persona) {
+        return modelMapper.map(persona, PersonaDTO.class);
+    }   
+
+    public void updateModelFromDTO(PersonaDTO personaDTO, Persona persona) {
+        modelMapper.map(personaDTO, persona);
+    }   
+
+
+    
+}
